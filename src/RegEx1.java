96f388dac6e0edb6763ce295ff3ed4a3597560c8
Dreder <dreder@icloud.com>
2019-10-23 00:18:41 +0300

    home work

--- src/RegEx1.java
+++ src/RegEx1.java
@@ -1,15 +1,15 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegExp1 {
+public class RegEx1 {
     public static void main(String[] args) {
 
         // имеются 2 строки. Найти вхождений
         // одной строки в другую (сколько раз встречается подстрока в строке)
 
-        String str = "Это тестовый текст для тестового поиска тестовой подстроки";
+        String str = "Это тестовый текст для тестового поиска тестовой подстроки. Тест";
         String str1 = "тест";
-        Pattern patt = Pattern.compile(str1);
+        Pattern patt = Pattern.compile(str1, Pattern.CASE_INSENSITIVE);
         Matcher matcher = patt.matcher(str);
         int i = 0;
         while (matcher.find()) {
