b9d4616cf936f675c0a2c2920f247b3dd026acf0
Dreder <dreder@icloud.com>
2019-10-23 02:37:57 +0300

    home work

--- src/OptionalRegEx/RegEx2.java
+++ src/OptionalRegEx/RegEx2.java
@@ -4,5 +4,9 @@ public class RegEx2 {
     public static void main(String[] args) {
         // даны два слова, состоящие из четного количества букв
         // Нужно составить третье слово из первой половины 1го и второй половины второго
+        String str = "пара слов";
+        String[] strings = str.split("\\s");
+        String str1 = strings[0].substring(0, strings[0].length()/2) + strings[1].substring(strings[1].length()/2);
+        System.out.println(str1);
     }
 }
--- src/OptionalRegEx/RegEx3.java
+++ src/OptionalRegEx/RegEx3.java
@@ -4,5 +4,16 @@ public class RegEx3 {
     public static void main(String[] args) {
         // найти самое длинное слово в предложении
         // (при условии, что все слова разной длинны)
+        String str = "Съешь ещё этих мягких французских булочек";
+        String[] strArr = str.split("\\s");
+        int length = strArr[0].length();
+        int pos = 0;
+        for (int i = 1; i < strArr.length; i++) {
+            if (strArr[i].length()>length) {
+                length = strArr[i].length();
+                pos = i;
+            }
+        }
+        System.out.println(strArr[pos]);
     }
 }
