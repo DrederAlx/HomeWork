f63d84e639df31e3eb9c4b7e9496321174037750
Dreder <dreder@icloud.com>
2019-10-23 00:34:52 +0300

    home work

---
+++ src/RegEx3.java
@@ -0,0 +1,6 @@
+public class RegEx3 {
+    public static void main(String[] args) {
+        // найти самое длинное слово в предложении
+        // (при условии, что все слова разной длинны)
+    }
+}
