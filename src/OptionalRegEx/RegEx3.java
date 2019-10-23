611f9583cad6c059a84c5a476f91b3a3d0b2c824
Dreder <dreder@icloud.com>
2019-10-23 03:13:27 +0300

    home work

--- src/OptionalRegEx/RegEx3.java
+++ src/OptionalRegEx/RegEx3.java
@@ -1,10 +1,18 @@
 package OptionalRegEx;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegEx3 {
     public static void main(String[] args) {
         // найти самое длинное слово в предложении
         // (при условии, что все слова разной длинны)
-        String str = "Съешь ещё этих мягких французских булочек";
+        String str = "Съешь! еще! этих! мягких? французских, булочек.";
+        Pattern pattern = Pattern.compile("[А-Яа-я ]");
+        Matcher matcher = pattern.matcher(str);
+        StringBuilder builder = new StringBuilder();
+        while (matcher.find()) builder.append(matcher.group());
+        str = builder.toString();
         String[] strArr = str.split("\\s");
         int length = strArr[0].length();
         int pos = 0;
