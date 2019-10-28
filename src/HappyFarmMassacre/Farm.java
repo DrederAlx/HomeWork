3f71ce8509572b0fad2ee591f44297e702a796c9
Dreder <dreder@icloud.com>
2019-10-28 22:00:05 +0300

    Added Farm

--- src/HappyFarmMassacre/Farm.java
+++ src/HappyFarmMassacre/Farm.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 public class Farm {
     private Home[] homeAnimals = new Home[10];
 
+    @Override
+    public String toString() {
+        return "Farm{" +
+                "homeAnimals=" + Arrays.toString(homeAnimals) +
+                '}';
+    }
+
     public void addAnimals() {
         int animalId;
         Home newAn = null;
@@ -14,15 +21,19 @@ public class Farm {
                 case 1:
                     newAn = new Cat(7, 15, "Кот", 10, 0, false);
                     this.homeAnimals[i] = newAn;
+                    break;
                 case 2:
                     newAn = new Cow(20, 5, "Корова", 15, 15, true);
                     this.homeAnimals[i] = newAn;
+                    break;
                 case 3:
                     newAn = new Rabbit(4, 20, "Кролик", 10, 10, true);
                     this.homeAnimals[i] = newAn;
+                    break;
                 case 4:
                     newAn = new Chicken(2, 10, "Курица", 5, 5, true);
                     this.homeAnimals[i] = newAn;
+                    break;
             }
         }
 
