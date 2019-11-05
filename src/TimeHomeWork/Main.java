4067fb5e0716a9bcf85e28773bba73cb81c8fea0
Dreder <dreder@icloud.com>
2019-11-05 23:39:35 +0300

    Done

---
+++ src/TimeHomeWork/Main.java
@@ -0,0 +1,38 @@
+package TimeHomeWork;
+
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        Есть три рабочие смены:
+
+        с 7:00 до 15:00
+        с 15:00 до 23:00
+        с 23:00 до 7:00
+        Определить какая сейчас смена (относительно текущего времени)
+
+        Наши занятия закончатся 20 января 2020 года. Сколько занятий осталось,
+        если они проходят 3 раза в неделю (пн, ср, пт). Праздничные дни не учитывать.
+
+ */
+
+        LocalTime currentTime = LocalTime.now();
+        LocalTime shiftOneBegin = LocalTime.of(7,0);
+        LocalTime shiftOneEnd = LocalTime.of(15,0);
+        LocalTime shiftTwoBegin = LocalTime.of(15,0);
+        LocalTime shiftTwoEnd = LocalTime.of(23,0);
+
+        if (currentTime.isAfter(shiftOneBegin) && currentTime.isBefore(shiftOneEnd) ||
+                currentTime.equals(shiftOneBegin)){
+            System.out.println("Сейчас первая смена");
+        }
+        else if (currentTime.isAfter(shiftTwoBegin) && currentTime.isBefore(shiftTwoEnd) ||
+                currentTime.equals(shiftTwoBegin)){
+            System.out.println("Сейчас вторая смена");
+        }
+        else {
+            System.out.println("Сейчас третья смена");
+        }
+    }
+}
