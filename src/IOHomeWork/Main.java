9942073e04fe0fdcce87313d41e2bd2e4fa50122
Dreder <dreder@icloud.com>
2019-11-19 21:38:48 +0300

    IO Home Work 2/4 done

--- src/IOHomeWork/Main.java
+++ src/IOHomeWork/Main.java
@@ -1,28 +1,32 @@
 package IOHomeWork;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
         //TODO Написать свои реализации InputStream и OutputStream, которые будут расширять FilterInputStream и
         // FilterOutputStream, в переопределяемых методах (write и read) необходимо шифровать и дешифровать данные
         // (использовать xor)
 
-        //TODO Есть файл, написать метод который берет файл и разбивает его на две части и одну часть записывает в один файл,
-        // вторую - в другой. Написать метод, который берет два файла на вход и записывает их в один файл
-
-        File file = new File("resources/iofile.txt");
+        File file = new File("resources/source.txt");
         try {
-            readByteArray(file);
+            splitFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File file1 = new File("resources/part1.txt");
+        File file2 = new File("resources/part2.txt");
+        try {
+            mergeFiles(file1, file2);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void readByteArray (File file) throws IOException {
-        File file1 = new File("resources/newiofile1.txt");
-        File file2 = new File("resources/newiofile2.txt");
+    public static void splitFile (File file) throws IOException {
+        File file1 = new File("resources/part1.txt");
+        File file2 = new File("resources/part2.txt");
         try (FileInputStream fileInputStream = new FileInputStream(file);
              FileOutputStream fileOutputStream1 = new FileOutputStream(file1);
              FileOutputStream fileOutputStream2 = new FileOutputStream(file2)){
@@ -39,6 +43,22 @@ public class Main {
                     else {
                         fileOutputStream2.write(buf, 0, data);
                     }
+            }
+        }
+    }
+
+    public static void mergeFiles (File file1, File file2) throws IOException {
+        File outputFile = new File("resources/mergeResult.txt");
+        try (FileInputStream input1 = new FileInputStream(file1);
+             FileInputStream input2 = new FileInputStream(file2);
+             FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+            SequenceInputStream sequenceInputStream = new SequenceInputStream(input1, input2);
+
+            byte[] buf = new byte[10];
+            int data;
+
+            while ((data = sequenceInputStream.read(buf)) > 0){
+                fileOutputStream.write(buf, 0, data);
             }
         }
     }
