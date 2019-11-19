7f9f18681f79a7d22e8bf8b48b33b8fd83ec8e14
Dreder <dreder@icloud.com>
2019-11-19 20:24:57 +0300

    IO Home Work 1/4 done

--- src/IOHomeWork/Main.java
+++ src/IOHomeWork/Main.java
@@ -1,5 +1,8 @@
 package IOHomeWork;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //TODO Написать свои реализации InputStream и OutputStream, которые будут расширять FilterInputStream и
@@ -9,5 +12,34 @@ public class Main {
         //TODO Есть файл, написать метод который берет файл и разбивает его на две части и одну часть записывает в один файл,
         // вторую - в другой. Написать метод, который берет два файла на вход и записывает их в один файл
 
+        File file = new File("resources/iofile.txt");
+        try {
+            readByteArray(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readByteArray (File file) throws IOException {
+        File file1 = new File("resources/newiofile1.txt");
+        File file2 = new File("resources/newiofile2.txt");
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             FileOutputStream fileOutputStream1 = new FileOutputStream(file1);
+             FileOutputStream fileOutputStream2 = new FileOutputStream(file2)){
+
+            byte[] buf = new byte[10];
+            int data;
+
+            int available = fileInputStream.available();
+            System.out.println(fileInputStream.available()/2);
+                while ((data = fileInputStream.read(buf)) > 0){
+                    if (fileInputStream.available() > available / 2){
+                        fileOutputStream1.write(buf, 0, data);
+                    }
+                    else {
+                        fileOutputStream2.write(buf, 0, data);
+                    }
+            }
+        }
     }
 }
