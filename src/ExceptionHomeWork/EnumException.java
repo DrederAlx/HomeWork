9bae53839cb266ebbd616a59b5c8432bf797c0b1
Dreder <dreder@icloud.com>
2019-11-27 01:40:15 +0300

    Exception HW 2/4 complete

--- src/ExceptionHomeWork/EnumException.java
+++ src/ExceptionHomeWork/EnumException.java
@@ -4,6 +4,19 @@ import java.io.FileNotFoundException;
 import java.nio.file.AccessDeniedException;
 import java.util.jar.JarException;
 
+       /* Метод static void throwException(Status status)
+       throws JarException, FileNotFoundException, AccessDeniedException
+       принимает на вход enum и выбрасывает следующие исключения в зависимости от значения status:
+       если status FILE_NOT_FOUND, выбрасывает FileNotFoundException
+       если status ACCESS_DENIED, выбрасывает AccessDeniedException
+       если status JAR_ERROR, выбрасывает JarException.
+       При вызове метода throwException обработать исключения следующим образом:
+       FileNotFoundException - выводить в консоль стек трейс, AccessDeniedException
+       - выводить в консоль сообщение исключения (метод getMessage()) и снова
+       выбрасывать exception, JarException - выводить в консоль стек трейс.
+
+       enum с необходимыми константами нужно создать.*/
+
 public class EnumException {
     static void throwException(Status status) throws JarException, FileNotFoundException, AccessDeniedException {
 
--- src/ExceptionHomeWork/Main.java
+++ src/ExceptionHomeWork/Main.java
@@ -8,19 +8,6 @@ public class Main {
     public static void main(String[] args){
         ExceptionList.generate();
 
-       /* Метод static void throwException(Status status)
-       throws JarException, FileNotFoundException, AccessDeniedException
-       принимает на вход enum и выбрасывает следующие исключения в зависимости от значения status:
-        если status FILE_NOT_FOUND, выбрасывает FileNotFoundException
-        если status ACCESS_DENIED, выбрасывает AccessDeniedException
-        если status JAR_ERROR, выбрасывает JarException.
-       При вызове метода throwException обработать исключения следующим образом:
-       FileNotFoundException - выводить в консоль стек трейс, AccessDeniedException
-        - выводить в консоль сообщение исключения (метод getMessage()) и снова
-        выбрасывать exception, JarException - выводить в консоль стек трейс.
-
-        enum с необходимыми константами нужно создать.*/
-
         try {
             EnumException.throwException(Status.ACCESS_DENIED);
 
@@ -36,5 +23,8 @@ public class Main {
         } catch (JarException e) {
             e.printStackTrace();
         }
+
+
+
     }
 }
