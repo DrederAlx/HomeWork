ba8c2f1132293a7e49b1b8f245487dbfdad5891d
Dreder <dreder@icloud.com>
2019-12-04 02:35:44 +0300

    Lambda/Stream HW 1/4 done

--- src/LambdaStreamHW/task3/Pupil.java
+++ src/LambdaStreamHW/task3/Pupil.java
@@ -1,6 +1,8 @@
 package LambdaStreamHW.task3;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 public class Pupil {
     enum Gender {
@@ -10,6 +12,61 @@ public class Pupil {
     private String name;
     private Gender gender;
     private LocalDate birth;
+
+    public Pupil(int number, String name, Gender gender, LocalDate birth) {
+        this.number = number;
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public int getAge() {
+        return (int) ChronoUnit.DAYS.between(getBirth(), LocalDate.now());
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Pupil{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", birth=" + birth +
+                '}';
+    }
 
     // TODO: добавить все необходимые методы
 
--- src/LambdaStreamHW/task3/PupilTask.java
+++ src/LambdaStreamHW/task3/PupilTask.java
@@ -1,14 +1,63 @@
 package LambdaStreamHW.task3;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class PupilTask {
     public static void main(String[] args) {
+
+        ArrayList<Pupil> pupils = new ArrayList<>();
+
+        pupils.add(new Pupil(1123, "James", Pupil.Gender.MALE, LocalDate.of(2006, Month.OCTOBER, 11)));
+        pupils.add(new Pupil(4353, "Olga", Pupil.Gender.FEMALE, LocalDate.of(2007, Month.DECEMBER, 4)));
+        pupils.add(new Pupil(8623, "Mike", Pupil.Gender.MALE, LocalDate.of(2008, Month.JULY, 24)));
+        pupils.add(new Pupil(9345, "Alice", Pupil.Gender.FEMALE, LocalDate.of(2008, Month.FEBRUARY, 27)));
+        pupils.add(new Pupil(7342, "Bob", Pupil.Gender.MALE, LocalDate.of(2008, Month.MARCH, 17)));
+        pupils.add(new Pupil(2574, "Helen", Pupil.Gender.FEMALE, LocalDate.of(2007, Month.MAY, 3)));
+        pupils.add(new Pupil(8871, "Richard", Pupil.Gender.MALE, LocalDate.of(2009, Month.APRIL, 21)));
+        pupils.add(new Pupil(9436, "Kurt", Pupil.Gender.MALE, LocalDate.of(2006, Month.JUNE, 28)));
+
         // Используя Stream API:
 
         // 1. Разделить учеников на две группы: мальчиков и девочек. Результат: Map<Pupil.Gender, ArrayList<Pupil>>
 
+        // вариант 1
+        Map<Pupil.Gender, ArrayList<Pupil>> genderMap = pupils.stream()
+                .collect(Collectors.groupingBy(Pupil::getGender, Collectors.toCollection(ArrayList::new)));
+
+        System.out.println(genderMap);
+
+        // вариант 2
+        pupils.stream()
+                .collect(Collectors.groupingBy(Pupil::getGender, Collectors.toCollection(ArrayList::new)))
+                .entrySet()
+                .forEach(System.out::println);
+
+
         // 2. Найти средний возраст учеников
 
+        int averageAge = (int) pupils.stream()
+                .mapToInt(Pupil::getAge)
+                .map(age -> age / 365)
+                .average()
+                .getAsDouble();
+
+        System.out.println(averageAge);
+
+
         // 3. Найти самого младшего ученика
+
+        Pupil minPupil = pupils.stream()
+                .min(Comparator.comparing(Pupil::getAge))
+                .orElse(new Pupil(1111, "Default", Pupil.Gender.MALE, LocalDate.of(2008, Month.JANUARY, 1)));
+
+        System.out.println(minPupil);
 
         // 4. Найти самого старшего ученика
 
