22a615498d790df4d49027fc0b00fd6088bc4aa9
Dreder <dreder@icloud.com>
2019-12-13 04:56:45 +0300

    Optional thread HW done

--- src/LambdaStreamHW/task1/StudentTask.java
+++ src/LambdaStreamHW/task1/StudentTask.java
@@ -1,6 +1,7 @@
 package LambdaStreamHW.task1;
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,17 +12,51 @@ public class StudentTask {
 
         Student student1 = new Student("Ivan", Arrays.asList("Math", "Physics", "Biology", "Philosophy"));
         Student student2 = new Student("Bob", Arrays.asList("Economics", "Physics", "Math"));
-        Student student3 = new Student("Mike", Arrays.asList("Math","Biology", "Biology"));
+        Student student3 = new Student("Mike", Arrays.asList("Physics","Math", "Biology"));
         Student student4 = new Student("John", Arrays.asList("Math", "Physics", "Biology"));
         Student student5 = new Student("Jesus", Arrays.asList("Philosophy", "Sociology"));
-        Student student6 = new Student("George", Arrays.asList("Math", "Physics", "Biology", "Philosophy"));
-        Student student7 = new Student("Rob", Arrays.asList("Math", "Physics", "Biology"));
+        Student student6 = new Student("George", Arrays.asList("Philosophy", "Physics", "Biology", "Math"));
+        Student student7 = new Student("Rob", Arrays.asList("Biology", "Physics", "Math"));
 
         Stream<Student> studentStream = Stream.of(student1, student2, student3, student4, student5, student6, student7);
 
+        // Map<List<String>, String> studentMap =
+        /*
         studentStream
-                .map(student -> Collectors.toMap(Student::getSubjects, Student::getLogin))
-                .flatMap()
+                .collect(Collectors.toMap(Student::getSubjects, Student::getLogin, (item1, item2) -> null))
+                .entrySet()
+                .stream()
+                .flatMap(map -> map.getKey().stream())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() == 1)
+                .map(e -> e.getKey())
+                .forEach(System.out::println);
+         */
+
+        /*
+        studentStream
+                .collect(Collectors.toMap(student -> student.getSubjects().stream().reduce(Objects::toString).get(),
+                        Function.identity(), (item1, item2) -> item1))
+                .entrySet()
+                .stream()
+                .forEach(System.out::println);
+
+         */
+
+        studentStream
+                .flatMap(student -> student.getSubjects().stream()
+                        .map(subjects -> new HashMap.SimpleEntry<>(subjects, student)))
+                .collect(Collectors.groupingBy(map -> map.getKey()) )
+                .entrySet().stream()
+                .filter(e -> e.getValue().size() == 1)
+                .map(e -> e.getValue().get(0).getValue().getLogin())
+                .forEach(System.out::println);
+
+
+
+
 
         }
     }
@@ -50,5 +85,13 @@ class Student {
 
     public void setSubjects(List<String> subjects) {
         this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "login='" + login + '\'' +
+                ", subjects=" + subjects +
+                '}';
     }
 }
